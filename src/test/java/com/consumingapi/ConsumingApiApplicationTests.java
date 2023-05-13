950a96a3e2a9b9c12a515635d25e70232d43caf4
shubh990 <singhalshubham990@gmail.com>
2023-05-13 19:19:16 +0530

    test changes

--- src/test/java/com/consumingapi/ConsumingApiApplicationTests.java
+++ src/test/java/com/consumingapi/ConsumingApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.consumingapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ConsumingApiApplicationTests.class)
 class ConsumingApiApplicationTests {
 
 	@Test
